56ff31bb1e63de2bc8054e88a673f7025c118615
Fabrizio Castillo <138902138+FabrizioCastillo@users.noreply.github.com>
2025-11-12 16:24:34 -0300

    Delete src directory

--- src/main/java/com/example/demo/gestioncursos/repository/EstudianteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.gestioncursos.repository;
-
-import com.example.demo.gestioncursos.model.Estudiante;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EstudianteRepository extends JpaRepository<Estudiante, Long> {}
--- src/main/java/com/example/demo/gestioncursos/repository/ProfesorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.gestioncursos.repository;
-
-import com.example.demo.gestioncursos.model.Profesor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProfesorRepository extends JpaRepository<Profesor, Long> {}
